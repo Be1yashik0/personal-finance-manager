e0d4ff53e3b0e638c8b3170996307061967a3406
ANikitin <anikitin@lanit.ru>
2025-12-17 22:27:22 +0300

    fix: Add minimal Spring Boot structure for CI

---
+++ backend/src/main/java/com/personalfinancemanager/PersonalFinanceManagerBackendApplication.java
@@ -0,0 +1,13 @@
+package com.personalfinancemanager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PersonalFinanceManagerBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PersonalFinanceManagerBackendApplication.class, args);
+    }
+
+}
